eee0c105c522f0212b03ff6521ac16870eac5f0b
Jenny Robertson <jenny.robertson@bbc.co.uk>
2020-02-07 15:36:50 +0000

    use js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('jen!');
+window.onload = () => {
+    document.getElementById("box").setAttribute("rotation", "30 30 29");
+    document.getElementById("box").setAttribute("rotation", "30 30 29");
+};
