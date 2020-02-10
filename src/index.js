1c9b707c4b7b02efd439fc206ae50513ae2db2d4
Jenny ☺ <jenny@jennyrobertson.net>
2020-02-10 09:44:35 +0000

    moo moo meadows

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 window.onload = () => {
+    AFRAME.registerComponent('log', {
+        schema: {
+          message: {type: 'string', default: 'Hello, World!'}
+        },
+      
+        init: function () {
+          console.log(this.data.message);
+        }
+      });
+    console.log('yo');
     document.getElementById("box").setAttribute("rotation", "30 30 29");
     document.getElementById("box").setAttribute("rotation", "30 30 29");
 };
